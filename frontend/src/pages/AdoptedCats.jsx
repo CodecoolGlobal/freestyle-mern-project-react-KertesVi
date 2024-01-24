88082e5d4fa4bdc38dbdfbd3710147d5fff0ce23
MMDora2 <m.mihicsdora@gmail.com>
2024-01-24 13:47:35 +0100

    success story

--- frontend/src/pages/AdoptedCats.jsx
+++ frontend/src/pages/AdoptedCats.jsx
@@ -3,27 +3,29 @@ import './AdoptedCats.css'; // Import the CSS file with the styling
 
 export default function AdoptedCats() {
   return (
+    <div  className='body'>
+    <div className='title'>Successful Adoption Story of the Month</div>
     <div className="container">
       <div className="card">
         <img src="https://upload.wikimedia.org/wikipedia/commons/d/d6/Cat_plotting_something_evil%21.jpg" alt="Plotting Cat" />
-        <div className="card__head">Plotting Cat</div>
+        <div className="card__head">Domino the elegant cat</div>
       </div>
       <div className="card">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/False_alarm_-a.jpg/1280px-False_alarm_-a.jpg" alt="Angry Cat" />
-        <div className="card__head">Angry Cat</div>
+        <img src="https://www.comfortzone.com/-/media/Project/OneWeb/ComfortZone/Images/Blog/adopting-an-older-cat.jpg" alt="Hug Cat" />
+        <div className="card__head">Honey in the owners hands</div>
       </div>
       <div className="card">
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Neugierige-Katze.JPG/1280px-Neugierige-Katze.JPG" alt="Curious Cat" />
-        <div className="card__head">Curious Cat</div>
+        <div className="card__head">Wonder Whiskey the curious Cat</div>
       </div>
       <div className="card">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Al_acecho_%289272124788%29.jpg/1280px-Al_acecho_%289272124788%29.jpg" alt="Prowling Cat" />
-        <div className="card__head">Prowling Cat</div>
+        <img src="https://d.newsweek.com/en/full/2221075/bonded-cats-melt-hearts-online.png?w=1600&h=1200&q=88&f=b0de66ddd8207f53d35bcebee1d87a45" alt="Brothers" />
+        <div className="card__head">Brothers Together: Leo & Oliver</div>
       </div>
       <div className="card">
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Mimi%26Tigsi.jpg/1280px-Mimi%26Tigsi.jpg" alt="Sleepy Cat" />
-        <div className="card__head">Sleepy Cat</div>
+        <div className="card__head">Luna the sleepy cat</div>
       </div>
-    </div>
+    </div></div>
   );
 }
