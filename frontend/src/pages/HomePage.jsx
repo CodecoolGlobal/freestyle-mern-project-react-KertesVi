03bb74d089193f6b579363bde5d4569d3a4e8e7e
KertesVi <kerteszviki@gmail.com>
2024-01-25 17:06:36 +0100

    fix css a little bit

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,45 +3,21 @@
 import React from "react";
 
 export default function HomePage(props) {
-    const { fullname } = props;
-  
-  
-      // useEffect(() => {
-    //   const fetchData = async () => {
-    //     try {
-    //       const response = await fetch("/api/signup");
-    //       if (!response.ok) {
-    //         console.error("Network response was not ok");
-    //       }
-    //       const data = await response.json();
-    //       console.log("Fetched data:", data);
-    //      } catch (error) {
-    //       console.error("Error fetching data:", error);
-    //     }
-    //   };
-    //   fetchData();
-    // }, []);
-  
-    return (
-  
-      <div className="HomePage">
-        {fullname ? (
-          <>
-        <h1>Hello {fullname}</h1>
-        <button>Adopt a cat</button>
-          <button>Donate for the shelter</button>
-          <button>Our cats</button>
-          <button>Adopted a cat</button>
-          </>
-          ): (
-            <>
-            <h1>Wellcome</h1>
-          <button>Adopt a cat</button>
-          <button>Donate for the shelter</button>
-          <button>Our cats</button>
-          <button>Adopted a cat</button>
-          </>
-          )}
-      </div>
-    );
-  }
+  const { fullname } = props;
+
+  return (
+    <div className="HomePage">
+      {fullname ? (
+        <>
+          <h1>Hello {fullname}</h1>
+          <p>... under construction</p>
+        </>
+      ) : (
+        <>
+          <h1>Wellcome</h1>
+          <p>... under construction</p>
+        </>
+      )}
+    </div>
+  );
+}
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,4 +1,4 @@
-// import "./LandingPage.scss";
+import "./LandingPage.scss";
 import { Link } from "react-router-dom";
 
 export default function LandingPage() {
