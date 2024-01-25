f6d0262861211cec7d8c82c71bcc4ad328fc670e
Iren Peter <peterirenkat@gmail.com>
2024-01-25 12:31:44 +0100

    fetch, getting closer

--- frontend/src/pages/OurCats.jsx
+++ frontend/src/pages/OurCats.jsx
@@ -1,40 +1,62 @@
-
 import React, { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
+// import BreedModal from "../Components/BreedModal";
 
-export default function OurCats(){
-    const [catData, setCatData] = useState(null);
+export default function OurCats() {
+  const [catData, setCatData] = useState(null);
+  // const [visible, setVisible] = useState(false)
 
-    useEffect(() => {
+  useEffect(() => {
     const fetchData = async () => {
-        try {
-          const response = await fetch("/api/cats");
-          const dataJson = await response.json();
-          
-          setCatData(dataJson);
-          console.log(dataJson);
-        } catch (error) {
-          console.log("Error fetching data", error);
-        }
-      };
-      fetchData();}, [])
-    return (
-        <div>
-            
+      try {
+        const response = await fetch("/api/cats");
+        const dataJson = await response.json();
 
-        <div className="catlist">
-     
-      {catData && catData.map((cat) => (
-        <div key={cat._id}>
-            <ul><li><p> <strong>Name: {cat.name} </strong></p></li>
-            <li>Breed {cat.breed}</li></ul>
-        </div>
-      ))}
+        setCatData(dataJson);
+        console.log(dataJson);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+
+  function showModal(event) {
+  //   setVisible(!visible)
+  //   return<BreedModal/>
+  // <Link to={"/breed"}/>
+console.log("breed is clicked");
+const breed = (event.target.value);
+console.log(breed);
+  }
+
+
+  return (
+    <div>
+      <div className="catlist">
+        {catData &&
+          catData.map((cat) => (
+            <div className="container" key={cat._id}>
+             
+                  <h4>Name: {cat.name} </h4>
+                <img src={cat.image} alt={cat.name}></img>
+                <p>{cat.sex}</p>
+                <p>Age: {cat.age}</p>
+                <p>Location: {cat.location}</p>
+                <p>Supported by {cat.dollars}$</p>
+                <p>Breed: {cat.breed}</p><button value={cat.breed} onClick={showModal}>Learn more about {cat.breed}</button>
+            </div>
+          ))}
+      </div>
     </div>
-        </div>
-     
-        
-         
-        
-   
-    )
+
+    //   /<div class="card">
+    //   <img src="img_avatar.png" alt="Avatar" style="width:100%">
+    //   <div class="container">
+    //     <h4><b>John Doe</b></h4>
+    //     <p>Architect & Engineer</p>
+    //   </div>
+    // </div>
+  );
 }
