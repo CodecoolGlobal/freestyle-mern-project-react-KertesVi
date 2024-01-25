7ce9af46063afdb4f5c6cddf63f3af93da80ec6f
KertesVi <kerteszviki@gmail.com>
2024-01-25 15:05:11 +0100

    merged homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,7 +2,6 @@
 // import { useEffect, useState } from "react";
 
 export default function HomePage(props) {
-<<<<<<< HEAD
     const {fullname} = props;
   
   
@@ -45,47 +44,3 @@ export default function HomePage(props) {
       </div>
     );
   }
-=======
-  const {fullname} = props;
-
-
-    // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await fetch("/api/signup");
-  //       if (!response.ok) {
-  //         console.error("Network response was not ok");
-  //       }
-  //       const data = await response.json();
-  //       console.log("Fetched data:", data);
-  //      } catch (error) {
-  //       console.error("Error fetching data:", error);
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
-
-  return (
-
-    <div className="HomePage">
-      {fullname ? (
-        <>
-      <h1>Hello {fullname}</h1>
-      <button>Adopt a cat</button>
-        <button>Donate for the shelter</button>
-        <button>Our cats</button>
-        <button>Adopted a cat</button>
-        </>
-        ): (
-          <>
-          <h1>Wellcome</h1>
-        <button>Adopt a cat</button>
-        <button>Donate for the shelter</button>
-        <button>Our cats</button>
-        <button>Adopted a cat</button>
-        </>
-        )}
-    </div>
-  );
-}
->>>>>>> 0eb28ad33590fe8bafa212ea18db17e8ca28baa9
