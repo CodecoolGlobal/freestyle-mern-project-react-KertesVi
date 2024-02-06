45d6ea4316964731068f1d2d71abe93fedefe96b
KertesVi <kerteszviki@gmail.com>
2024-02-06 14:01:05 +0100

    refactor

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React,from "react";
 import ReactDOM from "react-dom/client";
 import LandingPage from "./pages/LandingPage.jsx";
 // import "./index.css";
