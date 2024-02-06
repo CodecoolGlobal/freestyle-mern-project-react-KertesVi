fba072b33dfc0c6e0de7c3bfb0ac21e465b486c8
Zergi <bestestyasuo@gmail.com>
2024-02-06 09:34:02 +0100

    cat model changed back to previous version

--- backend/model/cat.js
+++ backend/model/cat.js
@@ -4,19 +4,24 @@ const { Schema, model } = mongoose;
 const catSchema = new Schema({
     name: {
         type: String,
+        required: true
     },
     sex: {
         type: String,
+        required: true
     },
     age: {
         type: Number,
+        required: true
     },
     location: {
         type: String,
+        required: true
     },
     picture: String,
     breed: {
         type: String,
+        required: true
     },
     dollars: {
         type: Number,
